56a49f924a8eed28c06f081a576d1d6692ea3db8
cyberpunkangel <149angel@hotmail.com>
2023-06-16 22:34:51 -0600

    Configuración para pruebas Jest

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -8,9 +8,9 @@ export const CalendarApp = () => {
   return (
     <Provider store={ store }>
       <BrowserRouter>
-        {/* <HashRouter> */}
-          <AppRouter />
-        {/* </HashRouter> */}
+      {/* <HashRouter> */}
+        <AppRouter />
+      {/* </HashRouter> */}
       </BrowserRouter>
     </Provider>
   )
